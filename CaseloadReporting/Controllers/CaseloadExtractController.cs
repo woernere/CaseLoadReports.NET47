7611d3fcbdd11ee67ea940f85a53728ede3bf487
Eric Woerner <WoernerE@courts.mi.gov>
2018-06-15 12:23:04 -0400

    initial2

--- CaseloadReporting/Controllers/CaseloadExtractController.cs
+++ CaseloadReporting/Controllers/CaseloadExtractController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+using CaseloadReporting.Controllers;
 
 namespace CaseloadReporting.Controllers
 {
---
+++ CaseloadReporting/Models/List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+
+namespace CaseloadReporting.Models
+{
+    public class List
+    {
+        [JsonProperty(PropertyName = "Count")]
+        public int Count { get; set; }
+        [JsonProperty(PropertyName = "GroupName")]
+        public string GroupName { get; set; }
+        [JsonProperty(PropertyName = "ActionName")]
+        public string ActionName { get; set; }
+    }
+}
