2c5db28a979b47937c413233dab85a5e073e9b07
Eric Woerner <WoernerE@courts.mi.gov>
2018-06-15 11:32:29 -0400

    initial commit

---
+++ CaseloadReporting/Controllers/CaseloadExtractController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaseloadReporting.Controllers
+{
+    public class CaseloadExtractController : Controller
+    {
+        // GET: CaseloadExtract
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
